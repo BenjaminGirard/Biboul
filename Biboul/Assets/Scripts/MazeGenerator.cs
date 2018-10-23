738c128af27e68c60eb008cc603cd07c7e989100
quentilius <33604487+qfm215@users.noreply.github.com>
2018-10-23 11:52:44 +0900

    Door between rooms

--- Biboul/Assets/Scripts/MazeGenerator.cs
+++ Biboul/Assets/Scripts/MazeGenerator.cs
@@ -8,7 +8,7 @@ public class MazeGenerator : MonoBehaviour {
     public int roomSize;
     public int mazeSize;
 
-    List<GameObject> mazeRooms;
+    public List<GameObject> mazeRooms = new List<GameObject>();
 
     private void GenerateMazeRooms()
     {
@@ -32,60 +32,9 @@ public class MazeGenerator : MonoBehaviour {
         }
     }
 
-    private void DigDoor(GameObject room, Vector3 pos)
-    {
-        while (room.GetComponent<RoomGenerator>().roomCubes.Count < roomSize * roomSize * 2 + (roomSize - 2) * roomSize * 2 + (roomSize - 2) * (roomSize - 2) * 2)
-        {
-        }
-        List<GameObject> cubes = room.GetComponent<RoomGenerator>().roomCubes;
-        foreach (var cube in cubes)
-        {
-            //            UnityEngine.Debug.Log(cube.transform.localPosition);
-            if (pos == cube.transform.localPosition)
-            {
-                UnityEngine.Debug.Log("in");
-//                cubes.Remove(cube);
-                GameObject.Destroy(cube);
-            }
-        }
-    }
-
-    private void GenerateDoors()
-    {
-        foreach (var room in mazeRooms)
-        {
-            System.Random rnd = new System.Random();
-            int face = 1; //rnd.Next(1, 6);
-            switch(face)
-            {
-                case 1:
-                    DigDoor(room, new Vector3(0, roomSize / 2, roomSize / 2));                    
-                    break;
-                case 2:
-                    DigDoor(room, new Vector3(roomSize / 2, 0, roomSize / 2));
-                    break;
-                case 3:
-                    DigDoor(room, new Vector3(roomSize / 2, roomSize / 2, 0));
-                    break;
-                case 4:
-                    DigDoor(room, new Vector3(roomSize - 1, roomSize / 2, roomSize / 2));
-                    break;
-                case 5:
-                    DigDoor(room, new Vector3(roomSize / 2, roomSize - 1, roomSize / 2));
-                    break;
-                case 6:
-                    DigDoor(room, new Vector3(roomSize / 2, roomSize / 2, roomSize - 1));
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
     // Use this for initialization
     void Start () {
-        mazeRooms = new List<GameObject>();
         GenerateMazeRooms();
-        GenerateDoors();
+        //GenerateDoors();
     }
 }
--- Biboul/Assets/Scripts/RoomGenerator.cs
+++ Biboul/Assets/Scripts/RoomGenerator.cs
@@ -7,7 +7,13 @@ public class RoomGenerator : MonoBehaviour {
     public GameObject cube;
     public int size;
 
-    public List<GameObject> roomCubes;
+    public List<GameObject> roomCubes = new List<GameObject>();
+    List<int> tempDoor = new List<int>();
+
+    public void AddDoor(int face)
+    {
+        tempDoor.Add(face);
+    }
 
     private void GenerateRoomCubes()
     {
@@ -30,10 +36,107 @@ public class RoomGenerator : MonoBehaviour {
         }
     }
 
+    private void DigDoor(Vector3 pos)
+    {
+        foreach (var cube in roomCubes)
+        {
+            //            UnityEngine.Debug.Log(cube.transform.localPosition);
+            if (pos == cube.transform.localPosition)
+            {
+                roomCubes.Remove(cube);
+                GameObject.Destroy(cube);
+                break;
+            }
+        }
+    }
+   
+    private void DigSide(int face, Vector3 sidePos)
+    {
+        List<GameObject> rooms = gameObject.GetComponentInParent<MazeGenerator>().mazeRooms;
+
+        foreach (var room in rooms)
+        {
+            if (sidePos == room.transform.localPosition)
+            {
+                room.GetComponent<RoomGenerator>().AddDoor(face);
+            }
+        }
+    }
+
+    private void GenerateDoor(int face)
+    {
+        Vector3 posit = this.transform.localPosition;
+        switch (face)
+        {
+            case 1:
+                DigDoor(new Vector3(0, size / 2, size / 2));
+                DigSide(4, new Vector3(posit.x - size, posit.y, posit.z));
+                break;
+            case 2:
+                DigDoor(new Vector3(size / 2, 0, size / 2));
+                DigSide(5, new Vector3(posit.x, posit.y - size, posit.z));
+                break;
+            case 3:
+                DigDoor(new Vector3(size / 2, size / 2, 0));
+                DigSide(6, new Vector3(posit.x, posit.y, posit.z - size));
+                break;
+            case 4:
+                DigDoor(new Vector3(size - 1, size / 2, size / 2));
+                DigSide(1, new Vector3(posit.x + size, posit.y, posit.z));
+                break;
+            case 5:
+                DigDoor(new Vector3(size / 2, size - 1, size / 2));
+                DigSide(2, new Vector3(posit.x, posit.y + size, posit.z));
+                break;
+            case 6:
+                DigDoor(new Vector3(size / 2, size / 2, size - 1));
+                DigSide(3, new Vector3(posit.x, posit.y, posit.z + size));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void GenerateTmpDoor(int face)
+    {
+        switch (face)
+        {
+            case 1:
+                DigDoor(new Vector3(0, size / 2, size / 2));
+                break;
+            case 2:
+                DigDoor(new Vector3(size / 2, 0, size / 2));
+                break;
+            case 3:
+                DigDoor(new Vector3(size / 2, size / 2, 0));
+                break;
+            case 4:
+                DigDoor(new Vector3(size - 1, size / 2, size / 2));
+                break;
+            case 5:
+                DigDoor(new Vector3(size / 2, size - 1, size / 2));
+                break;
+            case 6:
+                DigDoor(new Vector3(size / 2, size / 2, size - 1));
+                break;
+            default:
+                break;
+        }
+    }
+
     // Use this for initialization
     void Start () {
-        roomCubes = new List<GameObject>();
-
         GenerateRoomCubes();
+        System.Random rnd = new System.Random();
+        int face = 1;// rnd.Next(1, 6);
+        GenerateDoor(face);
 	}
+
+    void Update()
+    {
+        foreach (var tmp in tempDoor)
+        {
+            GenerateTmpDoor(tmp);
+        }
+    }
 }
