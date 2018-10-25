5ee173859a5ba764d8e1492d2158777d85d3ca67
quentilius <33604487+qfm215@users.noreply.github.com>
2018-10-25 17:08:47 +0900

    Add files via upload

---
+++ Biboul/Assets/Scripts/ClickSelectorGravity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSelectorGravity : MonoBehaviour {
+
+    public GameObject mainCamera;
+
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera");
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            AttractionSelector handle = mainCamera.GetComponent<AttractionSelector>();
+            if (handle.selected == null)
+            {
+                handle.selected = gameObject;
+            }
+            else
+            {
+                handle.selected.GetComponent<Rigidbody>().useGravity = false;
+                handle.selected.GetComponent<Gravity>().attractionObject = gameObject;
+                handle.selected = null;
+            }
+        }
+    }
+}
