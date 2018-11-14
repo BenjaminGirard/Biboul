50027efcf08f05b6dba4901ab06be871c934912b
margue_m <matteo.margueritte@epitech.eu>
2018-11-14 20:02:09 +0900

    drone enemy

---
+++ Biboul/Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+	private float speed;
+	public Transform target;
+	private bool isDestroyed;
+	private Animator anim;
+
+	public float Speed
+	{
+		get { return speed; }
+		set { speed = value; }
+	}
+
+	public Transform Target
+	{
+		get { return target; }
+		set { target = value; }
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = transform.GetComponent<Animator>();
+		Destroy(gameObject, 2);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
+	}
+
+	private void OnDestroy()
+	{
+		anim.Play("ProjectileClear");
+	}
+}
---
+++ Biboul/Assets/Scripts/DroneBehavior.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Effects;
+
+public class DroneBehavior : MonoBehaviour
+{
+	[SerializeField]
+	private Transform target;
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private float attackCooldown;
+
+	private bool isAggro;
+
+	private Animator anim;
+	public Transform bulletSpawn;
+	public GameObject bulletPrefab;
+	private Renderer droneEye;
+
+	private float timeStamp;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = transform.GetChild(0).GetChild(0).GetComponent<Animator>();
+		droneEye = transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Renderer>();
+		timeStamp = Time.time + attackCooldown;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Vector3.Distance(transform.position, target.position) < 12)
+		{
+			isAggro = true;
+			droneEye.material.color = Color.red;
+		}
+		else
+		{
+			isAggro = false;
+			droneEye.material.color = new Color(80,80,80);
+		}
+		if (isAggro)
+		{
+			transform.LookAt(target);
+			if (Vector3.Distance(transform.position, target.position) < 8)
+			{
+				if (timeStamp <= Time.time)
+				{
+					anim.Play("Attack");
+					timeStamp = Time.time + attackCooldown;
+				}
+			}
+			else
+ 			{
+				anim.Play("Move");
+				transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);	
+			}
+		}
+	}
+
+	public void Attack()
+	{
+		GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+		bullet.GetComponent<BulletBehavior>().Target = target;
+		bullet.GetComponent<BulletBehavior>().Speed = 6;
+	}
+}
